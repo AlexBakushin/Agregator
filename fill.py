f28f8f8f8c2e0b9406164cce4dbbb00434137095
AlexBakushin <bakushin35@gmail.com>
2024-04-16 21:19:40 +0500

    Add descriptions

--- fill.py
+++ fill.py
@@ -5,16 +5,29 @@ import bson
 
 
 async def main(inp):
+    """
+    Главная функция бота
+    :param inp: json с параметрами запроса
+    :return:  json с ответом
+    """
+    # Преобразование входящих данных
     dt_from_obj = datetime.datetime.strptime(inp["dt_from"], "%Y-%m-%dT%H:%M:%S")
     dt_to_obj = datetime.datetime.strptime(inp["dt_upto"], "%Y-%m-%dT%H:%M:%S")
+    # Пустой список для хранения !необходимых! данных
     right_data = []
+    # Пустой словарь для ответа
     answer = {"dataset": [], "labels": []}
+
+    #  Открываем файл с готовыми данными
     with open('sample_collection.bson', 'rb') as f:
         data = bson.decode_all(f.read())
+
+        # Фильтруем данные
         for i in data:
             if dt_from_obj <= i["dt"] <= dt_to_obj:
                 right_data.append(i)
 
+        # Если валидация для месяца
         if inp["group_type"] == "month":
             val = 0
             for j in range(dt_to_obj.month - dt_from_obj.month + 1):
@@ -27,6 +40,7 @@ async def main(inp):
                 answer["labels"].append(
                     f"{datetime.datetime(year=dt_from_obj.year, month=dt_from_obj.month + j, day=1, hour=0).isoformat()}")
 
+        # Если валидация для недели
         if inp["group_type"] == "week":
             val = 0
             week_start = dt_from_obj
@@ -43,6 +57,7 @@ async def main(inp):
                 week_start += datetime.timedelta(days=7)
                 week_end += datetime.timedelta(days=7)
 
+        # Если валидация для дня
         if inp["group_type"] == "day":
             val = 0
             for j in range((dt_to_obj - dt_from_obj).days + 1):
@@ -54,6 +69,7 @@ async def main(inp):
                 val = 0
                 answer["labels"].append((dt_from_obj + datetime.timedelta(days=j)).isoformat())
 
+        # Если валидация для часa
         if inp["group_type"] == "hour":
             val = 0
             for j in range(round((dt_to_obj - dt_from_obj).total_seconds() / 3600) + 1):
